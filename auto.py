0ab5561f9c8ea317b5b252095d07e7dee1dded46
xcollantes <xcollantes@users.noreply.github.com>
2018-09-09 15:47:53 -0700

    Add image recognition

--- auto.py
+++ auto.py
@@ -14,12 +14,25 @@ def moveMouse():
 def changeWin():
 
 
+def pressKey():
+	
+
+
+def findImg():
 
 if __name__ == '__main__':
 	try:
 		mouseMove()
 		changeWin()
 		pressKey()
+		os.chdir('lib/')
+		i = pyautogui.locateOnScreen('word.PNG')
+		
+		if i != None:
+			pyautogui.click(pyautogui(center(i)))
+		else:
+			print("Image not found.")
+		
 		
 	except KeyboardInterrupt as kb:
 		print("PROGRAM HALTED. %s" %kb)
