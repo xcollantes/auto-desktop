79f1f042c172ebce432e0599913302d67e722f40
xcollantes <xcollantes@users.noreply.github.com>
2018-09-09 16:54:54 -0700

    Added subtle key

--- auto.py
+++ auto.py
@@ -8,9 +8,7 @@ pyautogui.PAUSE = 0.80
 pyautogui.FAILSAFE = True
 
 # MoveMouse will run until ctl+c is pressed or failsafe is triggered 
-def moveMouse():
-	print ("Running... ")
-	print ("Press `CTL + C` on your keyboard to end this program.")
+def moveMouse():	
 	screenW = pyautogui.size()[0]
 	screenH = pyautogui.size()[1]
 	halfPos = (screenW // 2, screenH // 2)
@@ -22,14 +20,26 @@ def moveMouse():
 	except KeyboardInterrupt as kb:
 		print("PROGRAM HALTED.")
 
+		
+def pressKeySub(key='ctl'):
+	try:
+		while True:
+			pyautogui.press(key)
+			time.sleep(random.randrange(10, 25))
+	except KeyboardInterrupt as kb:
+		print("PROGRAM HALTED.")
+	
+	
 	
 if __name__ == '__main__':
 	try:
+		os.chdir('img/')
 		print("Hello! Thank you for using this productivity app.")
 		moveMouse()
-		#changeWin()
-		#pressKey()
-		os.chdir('img/')
+		pressKeySub(key='ctl')
+		print ("Running... ")
+		print ("Press `CTL + C` on your keyboard to end this program.")
+		
 		
 	except KeyboardInterrupt as kb:
 		print("PROGRAM HALTED. %s" %kb)
