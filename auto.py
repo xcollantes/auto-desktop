7e648094eefe8256ada7a272096a7f90b36f07e9
xcollantes <xcollantes@users.noreply.github.com>
2018-09-09 22:36:32 -0700

    Change target button

--- auto.py
+++ auto.py
@@ -4,11 +4,14 @@
 
 import pyautogui, time, arrow, os, random
 
+
 pyautogui.PAUSE = 0.80
 pyautogui.FAILSAFE = True
 
+
 # MoveMouse will run until ctl+c is pressed or failsafe is triggered 
 def moveMouse():	
+	running()
 	screenW = pyautogui.size()[0]
 	screenH = pyautogui.size()[1]
 	halfPos = (screenW // 2, screenH // 2)
@@ -22,23 +25,28 @@ def moveMouse():
 
 		
 def pressKeySub(key='ctl'):
+	running()
 	try:
 		while True:
 			pyautogui.press(key)
-			time.sleep(random.randrange(10, 25))
+			time.sleep(random.randrange(10, 20))
 	except KeyboardInterrupt as kb:
 		print("PROGRAM HALTED.")
 	
+
+# Indicator print out program is running 
+def running():
+	print ("Running... ")
+	print ("Press `CTL + C` on your keyboard to end this program.")
 	
 	
 if __name__ == '__main__':
 	try:
 		os.chdir('img/')
 		print("Hello! Thank you for using this productivity app.")
-		moveMouse()
-		pressKeySub(key='ctl')
-		print ("Running... ")
-		print ("Press `CTL + C` on your keyboard to end this program.")
+		
+		#moveMouse()
+		pressKeySub()
 		
 		
 	except KeyboardInterrupt as kb:
